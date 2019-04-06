62a750a0b83bf01df8969231ae9add79faec9379
DarkFlare <flaregamingxx@gmail.com>
2019-04-06 15:03:37 -0500

    Initial commit

---
+++ UTF-Converter/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace UTF_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(219, 23);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(177, 163);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data size:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(5, 135);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 20);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "-";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(5, 112);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(158, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Seperate each 2 bytes with:";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(5, 89);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Little Endian";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 16);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(55, 17);
+            this.radioButton1.TabIndex = 8;
+            this.radioButton1.Text = "UTF-8";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 55);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(61, 17);
+            this.radioButton3.TabIndex = 10;
+            this.radioButton3.Text = "UTF-32";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(4, 36);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(61, 17);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "UTF-16";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(219, 229);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(177, 32);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "<-- Convert to Text";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 8);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Hex:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Text:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 190);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 32);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Convert to Hex -->";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(400, 23);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 273);
+            this.textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(11, 23);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 273);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 302);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 36);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Load text file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 302);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(205, 36);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Load big endian UTF-16 encoded file";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(616, 349);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "UTF Converter by DarkFlare";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ UTF-Converter/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UTF_Converter
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public String ChangeBytes32(String input)
+        {
+            input = input.Replace(" ", "");
+            String output = "";
+            for (int i = 0; i < input.Length; i += 8)
+            {
+                int tmp = Convert.ToInt32(input.Substring(i, 8), 16);
+                int reversedBytes = System.Net.IPAddress.NetworkToHostOrder(tmp);
+                String start = reversedBytes.ToString("X").PadLeft(8, '0');
+                output += start;
+            }
+            return output;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte[] bytes = { };
+            int characters = 0;
+            if (radioButton1.Checked)
+            {
+                bytes = Encoding.UTF8.GetBytes(textBox1.Text);
+                characters = 2;
+            }
+            else if (radioButton2.Checked && !checkBox1.Checked)
+            {
+                bytes = Encoding.BigEndianUnicode.GetBytes(textBox1.Text);
+                characters = 4;
+            }
+            else if (radioButton2.Checked && checkBox1.Checked)
+            {
+                bytes = Encoding.Unicode.GetBytes(textBox1.Text);
+                characters = 4;
+            }
+            else if (radioButton3.Checked)
+            {
+                bytes = Encoding.UTF32.GetBytes(textBox1.Text);
+                characters = 8;
+            }
+            textBox2.Text = BitConverter.ToString(bytes).Replace("-", "");
+            textBox2.Text = ((radioButton3.Checked && !checkBox1.Checked) ? ChangeBytes32(textBox2.Text) : textBox2.Text);
+            if (checkBox2.Checked)
+                textBox2.Text = Regex.Replace(textBox2.Text, ".{" + characters + "}", "$0" + textBox3.Text);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = false;
+            checkBox2.Text = "Seperate each byte with:";
+            button3.Text = "Load UTF-8 encoded file";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = true;
+            checkBox2.Text = "Seperate each 2 bytes with:";
+            button3.Text = (checkBox1.Checked ? "Load little endian UTF-16 encoded file" : "Load big endian UTF-16 encoded file");
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = true;
+            checkBox2.Text = "Seperate each 4 bytes with:";
+            button3.Text = (checkBox1.Checked ? "Load little endian UTF-32 encoded file" : "Load big endian UTF-32 encoded file");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) // i would love to do a switch statement but sadly these are all booleans so it's not practical. this is the most effective way to do it
+                button3.Text = "Load UTF-8 encoded file";
+            else if (radioButton2.Checked && checkBox1.Checked)
+                button3.Text = "Load little endian UTF-16 encoded file";
+            else if (radioButton3.Checked && checkBox1.Checked)
+                button3.Text = "Load little endian UTF-32 encoded file";
+            else if (radioButton2.Checked && !checkBox1.Checked)
+                button3.Text = "Load big endian UTF-16 encoded file";
+            else
+                button3.Text = "Load big endian UTF-32 encoded file";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogue = new OpenFileDialog();
+            dialogue.Title = "Open Text File";
+            textBox1.Text = "Opening...";
+            if (dialogue.ShowDialog() != DialogResult.OK)
+                textBox1.Text = "";
+            else
+                textBox1.Text = File.ReadAllText(dialogue.FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialogue = new OpenFileDialog();
+            dialogue.Title = "Open UTF File";
+            textBox1.Text = "Opening...";
+            if (dialogue.ShowDialog() != DialogResult.OK)
+            {
+                textBox1.Text = textBox2.Text = "";
+            }
+            else
+            {
+                FileStream fs = new FileStream(dialogue.FileName, FileMode.Open);
+                int input;
+                textBox1.Text = "";
+                textBox2.Text = "";
+                for (int i = 0; (input = fs.ReadByte()) != -1; i++)
+                {
+                    String hex = string.Format("{0:X2}", input);
+                    textBox2.Text += hex;
+                }
+            }
+        }
+
+        private byte[] FromHex(string hex)
+        {
+            hex = hex.Replace(textBox3.Text, "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            byte[] data = ((radioButton3.Checked && !checkBox1.Checked) ? FromHex(ChangeBytes32(textBox2.Text)) : FromHex(textBox2.Text));
+            if (radioButton1.Checked)
+            {
+                textBox1.Text = Encoding.ASCII.GetString(data);
+            }
+            else if (radioButton2.Checked && checkBox1.Checked)
+            {
+                textBox1.Text = Encoding.Unicode.GetString(data);
+            }
+            else if (radioButton2.Checked && !checkBox1.Checked)
+            {
+                textBox1.Text = Encoding.BigEndianUnicode.GetString(data);
+            }
+            else if (radioButton3.Checked)
+            {
+                textBox1.Text = Encoding.UTF32.GetString(data);
+            }
+        }
+    }
+}
